39f0d3fa1bf3bedb7cf42f78ca61cc7ca204e80a
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-12-28 19:39:05 -0300

    Finished the first version of Client class

--- src/main/java/br/com/lucasbertoloto/desafiodio/model/Client.java
+++ src/main/java/br/com/lucasbertoloto/desafiodio/model/Client.java
@@ -1,15 +1,69 @@
 package br.com.lucasbertoloto.desafiodio.model;
 
+import br.com.lucasbertoloto.desafiodio.exception.*;
 import br.com.lucasbertoloto.desafiodio.model.account.Account;
-
+import br.com.lucasbertoloto.desafiodio.model.account.CheckingAccount;
+import br.com.lucasbertoloto.desafiodio.model.account.SavingAccount;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
-    public Client(String name) {
+    protected static long LAST_IDENTIFICATION;
+    private final long identification;
+    private String name;
+    private final List<Account> accounts;
 
+    public Client(String name) {
+        this.name = name;
+        identification = LAST_IDENTIFICATION++;
+        accounts = new ArrayList<>();
+    }
+
+    public long getIdentification() {
+        return identification;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public List<Account> getAccounts() {
+        return accounts;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addAccount(Account account) throws MaxNumberQuantityAccountsReachedException,
+            MaxNumberQuantitySavingAccountsReachedException, MaxNumberQuantityCheckingAccountsReachedException,
+            AddingSameAccountException, AccountWithAnotherClientException {
+        if (accounts.size() >= 2)
+            throw new MaxNumberQuantityAccountsReachedException();
+        else {
+            for (Account ac : accounts) {
+                if (account.equals(ac))
+                    throw new AddingSameAccountException();
+                else if (ac instanceof SavingAccount && account instanceof SavingAccount)
+                    throw new MaxNumberQuantitySavingAccountsReachedException();
+                else if (ac instanceof CheckingAccount && account instanceof CheckingAccount)
+                    throw new MaxNumberQuantityCheckingAccountsReachedException();
+            }
+            if (!account.getClient().equals(this))
+                throw new AccountWithAnotherClientException();
+            accounts.add(account);
+        }
+    }
+
+    public Account removeAccount(Account account) throws NoAccountException, AccountNotFoundException {
+        if (accounts.isEmpty())
+            throw new NoAccountException();
+        else {
+            for (int i = 0; i < accounts.size(); i++) {
+                if (accounts.get(i).equals(account))
+                    return accounts.remove(i);
+            }
+            throw new AccountNotFoundException();
+        }
     }
 }
--- src/test/java/br/com/lucasbertoloto/desafiodio/model/ClientTest.java
+++ src/test/java/br/com/lucasbertoloto/desafiodio/model/ClientTest.java
@@ -1,5 +1,6 @@
 package br.com.lucasbertoloto.desafiodio.model;
 
+import br.com.lucasbertoloto.desafiodio.exception.*;
 import br.com.lucasbertoloto.desafiodio.model.account.Account;
 import br.com.lucasbertoloto.desafiodio.model.account.CheckingAccount;
 import br.com.lucasbertoloto.desafiodio.model.account.SavingAccount;
@@ -12,28 +13,62 @@ import java.util.List;
 
 public class ClientTest {
     private Client client;
+    private Account ac1;
+    private Account ac2;
 
     @BeforeEach
-    void initialize(){
+    void initialize() throws MaxNumberQuantityCheckingAccountsReachedException,
+            MaxNumberQuantityAccountsReachedException, MaxNumberQuantitySavingAccountsReachedException,
+            AddingSameAccountException, AccountWithAnotherClientException {
         client = new Client("Maycon");
-        Account ac1 = new SavingAccount(client);
-        Account ac2 = new CheckingAccount(client);
-
-        List<Account> accountList = new ArrayList<Account>();
-        accountList.add(ac1);
-        accountList.add(ac2);
+        ac1 = new SavingAccount(client);
+        ac2 = new CheckingAccount(client);
     }
 
     @Test
-    void shouldReturnAllAccountsOfThisClient() {
-        Account ac1 = new SavingAccount(client);
-        Account ac2 = new CheckingAccount(client);
-
-        List<Account> accountList = new ArrayList<Account>();
+    void shouldReturnAllAccountsOfThisClient(){
+        List<Account> accountList = new ArrayList<>();
         accountList.add(ac1);
         accountList.add(ac2);
-
         Assertions.assertTrue(client.getAccounts().equals(accountList));
+    }
+
+    @Test
+    void shouldReturnAnAccountWithAnotherClientException(){
+        Client c1 = new Client("Jose");
+        Assertions.assertThrows(AccountWithAnotherClientException.class, () -> c1.addAccount(ac1));
+    }
+
+    @Test
+    void shouldRemoveAnAccountOfThisClient() throws NoAccountException, AccountNotFoundException {
+        List<Account> accounts = new ArrayList<>(client.getAccounts());
+        Account account = accounts.remove(0);
+        System.out.println(account);
+        System.out.println(ac1);
+        Account removed = client.removeAccount(ac1);
+        System.out.println(removed);
+        Assertions.assertEquals(account,removed);
+        Assertions.assertEquals(accounts,client.getAccounts());
+
+        account = accounts.remove(0);
+        removed = client.removeAccount(ac2);
+        Assertions.assertEquals(account,removed);
+        Assertions.assertEquals(accounts,client.getAccounts());
+        Assertions.assertTrue(client.getAccounts().isEmpty());
+    }
+
+    @Test
+    void shouldReturnANoAccountException(){
+        client = new Client("Julio");
+        Assertions.assertThrows(NoAccountException.class, () -> client.removeAccount(ac1));
+    }
+
+    @Test
+    void shouldReturnAnAccountNotFoundException() throws MaxNumberQuantityCheckingAccountsReachedException,
+            MaxNumberQuantityAccountsReachedException, MaxNumberQuantitySavingAccountsReachedException,
+            AddingSameAccountException, AccountWithAnotherClientException {
+        Account account = new CheckingAccount(new Client("Julio"));
+        Assertions.assertThrows(AccountNotFoundException.class, () -> client.removeAccount(account));
     }
 
 }
