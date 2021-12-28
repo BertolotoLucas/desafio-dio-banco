a3826ab9a36176163d2a8498bcad7d3ba5172fcc
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-12-28 16:26:34 -0300

    Finished the first version of Account classes

--- src/main/java/br/com/lucasbertoloto/desafiodio/model/account/Account.java
+++ src/main/java/br/com/lucasbertoloto/desafiodio/model/account/Account.java
@@ -1,15 +1,55 @@
 package br.com.lucasbertoloto.desafiodio.model.account;
 
+import br.com.lucasbertoloto.desafiodio.exception.InsufficientBalanceException;
+import br.com.lucasbertoloto.desafiodio.exception.NegativeValueException;
+import br.com.lucasbertoloto.desafiodio.exception.NoValueException;
+import br.com.lucasbertoloto.desafiodio.model.Client;
+
+import java.util.Objects;
+
 public abstract class Account {
-    public void deposit(double v) {
-        //TODO
+    protected static long IDENTIFICATION;
+    protected static final int AGENCY = 1;
+    protected Client client;
+    protected Double balance;
+
+    public static long getIDENTIFICATION() {
+        return IDENTIFICATION;
     }
 
-    public void withdraw(double v) {
-        //TODO
+    public Client getClient() {
+        return client;
     }
 
-    public double getBalance() {
-        //TODO
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void deposit(Double value) throws NoValueException, NegativeValueException {
+        int i = value.compareTo(0D);
+        if (i == 0)
+            throw new NoValueException();
+        else if (i < 0)
+            throw new NegativeValueException();
+        else {
+            balance = balance + value;
+        }
+    }
+
+    public Double withdraw(Double value) throws NoValueException, NegativeValueException, InsufficientBalanceException {
+        int i = value.compareTo(0D);
+        if (i == 0)
+            throw new NoValueException();
+        else if (i < 0)
+            throw new NegativeValueException();
+        else {
+            int j = value.compareTo(balance);
+            if(j > 0)
+                throw new InsufficientBalanceException();
+            else {
+                balance = balance - value;
+                return value;
+            }
+        }
     }
 }
--- src/main/java/br/com/lucasbertoloto/desafiodio/model/account/CheckingAccount.java
+++ src/main/java/br/com/lucasbertoloto/desafiodio/model/account/CheckingAccount.java
@@ -2,8 +2,43 @@ package br.com.lucasbertoloto.desafiodio.model.account;
 
 import br.com.lucasbertoloto.desafiodio.model.Client;
 
-public class CheckingAccount extends Account{
-    public CheckingAccount(Client client) {
+import java.util.Objects;
 
+public class CheckingAccount extends Account{
+    private final long identification;
+
+    public CheckingAccount(Client client) {
+        this.client = client;
+        this.balance = 0D;
+        IDENTIFICATION = IDENTIFICATION + 1;
+        this.identification =  IDENTIFICATION;
     }
+
+    public long getIdentification() {
+        return identification;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CheckingAccount that = (CheckingAccount) o;
+        return identification == that.identification;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identification);
+    }
+
+    @Override
+    public String toString() {
+        return "CheckingAccount{" +
+                "identification=" + identification +
+                ", agency=" + AGENCY +
+                ", client=" + client.getNome() +
+                ", balance=" + balance +
+                '}';
+    }
+
 }
